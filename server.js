a056395650b5a4828a95d3e584bf116fb0bf30c3
micbuffa <micbuffa@gmail.com>
2024-02-28 13:59:49 +0100

    first working version

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ mongoose.Promise = global.Promise;
 //mongoose.set('debug', true);
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
-const uri = 'mongodb+srv://mb:P7zM3VePm0caWA1L@cluster0.zqtee.mongodb.net/assignments?retryWrites=true&w=majority';
-
+//const uri = 'mongodb+srv://mb:P7zM3VePm0caWA1L@cluster0.zqtee.mongodb.net/assignments?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://user:password@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
